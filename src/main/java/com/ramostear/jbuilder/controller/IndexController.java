aab2858875a18a720bdb20a436bd73f80cfdece8
13648362969 <cs2231808>
2017-07-07 14:58:05 +0800

    前端首页加载商品分类和商品列表

--- src/main/java/com/ramostear/jbuilder/controller/IndexController.java
+++ src/main/java/com/ramostear/jbuilder/controller/IndexController.java
@@ -11,7 +11,10 @@
 package com.ramostear.jbuilder.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,10 +23,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ramostear.jbuilder.entity.ScenicSpot;
 import com.ramostear.jbuilder.entity.Ticket;
 import com.ramostear.jbuilder.entity.TicketAttachment;
+import com.ramostear.jbuilder.entity.TicketGroup;
 import com.ramostear.jbuilder.kit.ReqDto;
+import com.ramostear.jbuilder.service.ScenicSpotService;
 import com.ramostear.jbuilder.service.TicketAttachmentService;
+import com.ramostear.jbuilder.service.TicketGroupService;
 import com.ramostear.jbuilder.service.TicketService;
 import com.ramostear.jbuilder.service.UserService;
 
@@ -42,10 +49,71 @@ public class IndexController {
 	private TicketService ticketService;
 	@Autowired
 	private TicketAttachmentService ticketAttachmentService;
-
+	@Autowired
+	private ScenicSpotService scenicSpotService;
+	@Autowired
+	private TicketGroupService ticketGroupService;
+	
 	@RequestMapping(value = { "/", "", "/index", "/index.html", "/index.htm" }, method = RequestMethod.GET)
-	public String index() {
-
+	public String index(Model model) {
+		
+		List<Ticket> tlist=this.ticketService.findAll();
+		List<TicketGroup> tgList = this.ticketGroupService.findAll();
+		List<ScenicSpot> sclist = this.scenicSpotService.findAll();
+		
+		Map<Long,TicketGroup> tgMap=new HashMap<>();
+        for(TicketGroup entity : tgList){
+        	tgMap.put(entity.getId(),entity);
+        }
+        
+        Map<Long,ScenicSpot> scMap=new HashMap<>();
+        for(ScenicSpot entity : sclist){
+        	scMap.put(entity.getId(),entity);
+        }
+        
+        List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+        
+        for(TicketGroup entity : tgList){
+        	
+        	Map<String,Object> showMap=new HashMap<>();
+        	Map<Long,ScenicSpot> forscMap=new HashMap<>();
+        	Map<Long,List<Ticket>> tempTicket=new HashMap<>();
+        	
+        	showMap.put("ticketGroup", entity.getId());
+        	showMap.put("forscMap", forscMap);
+        	showMap.put("tempTicket", tempTicket);
+        	
+        	//找出分组中的景区
+        	for(Ticket tEntity : tlist){
+        		
+        		Long tempScId=tEntity.getScenicId();
+        		
+        		//不在当前分组
+        		if(entity.getId()!=tEntity.getGroupId())
+        			continue;
+        		
+        		//已经存在此景区
+        		if(forscMap.containsKey(tempScId)){
+        			
+        			if(tempTicket.get(tempScId).size()<6)
+        				tempTicket.get(tempScId).add(tEntity);
+        			continue;
+        		}
+        		
+        		List<Ticket> ttlist=new ArrayList<Ticket>();
+        		ttlist.add(tEntity);
+        		tempTicket.put(tempScId, ttlist);
+        		forscMap.put(tempScId, scMap.get(tempScId));
+        	}
+        	if(forscMap.size()==0)
+        		continue;
+        	
+        	list.add(showMap);
+        }
+        model.addAttribute("tgMap",tgMap);
+        model.addAttribute("list",list);
+        
+        
 		return "index/index";
 	}
 
