ec5652990b6e91d11f6f26cc6719a7b46d260fb7
ramostear <ramostear@163.com>
2017-08-01 15:32:46 +0800

    s

---
+++ src/main/java/com/ramostear/jbuilder/test/DataSourceMap.java
@@ -0,0 +1,21 @@
+/**   
+													The Apache License 2.0
+												  Copyright (c) 2016 Ramostear 
+								                              2017年7月21日 下午3:13:08
+								Unless required by applicable law or agreed to in writing, software
+								 distributed under the License is distributed on an "AS IS" BASIS,
+							  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+								See the License for the specific language governing permissions and
+								                 limitations under the License. 
+*/
+package com.ramostear.jbuilder.test;
+
+/** 
+ * @Desc: () 
+ * @author: 谭朝红 
+ * @date: 2017年7月21日 下午3:13:08 
+ * @email:ramostear@163.com 
+ */
+public class DataSourceMap {
+
+}
